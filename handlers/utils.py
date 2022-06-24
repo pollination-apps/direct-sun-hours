536b1d70ad19f22f80b6e5ae100a8a37a143e4db
Antonello Di Nunzio <plugin@antonellodinunzio.online>
2022-06-24 14:17:12 +0200

    feat(skp): use new sensor grid workflow

--- handlers/utils.py
+++ handlers/utils.py
@@ -1,7 +1,24 @@
 """A module to collect utility functions."""
-
+import json
 from ladybug_geometry.geometry3d.face import Face3D
+from ladybug_geometry.geometry3d.mesh import Mesh3D
 from honeybee_radiance.sensorgrid import SensorGrid
+
+def create_grid_from_mesh(meshes):
+    error_msg = None
+    grids = []
+    for i, m in enumerate(meshes):
+        try:
+            dictionary = json.loads(m)
+            mesh = Mesh3D.from_dict(dictionary)
+            grid = SensorGrid.from_mesh3d(
+                f'skp-sensor-grid-{i}', mesh)
+        except Exception as e:
+            error_msg = e
+            grid = None
+        grids.append(grid)            
+    
+    return grids, error_msg
 
 def create_grid_from_faces(
     faces,
