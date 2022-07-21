569eb299f3d983838cfa64f40d5fbae68f834378
Antonello Di Nunzio <plugin@antonellodinunzio.online>
2022-07-21 17:16:38 +0200

    fix(output): input text up

--- simulation.py
+++ simulation.py
@@ -1,5 +1,6 @@
 """ A module to run local calculation. """
 
+from pollination_streamlit.selectors import job_selector
 import json
 import pathlib
 import streamlit as st
@@ -13,7 +14,6 @@ from honeybee_radiance.config import folders as rad_folders
 from pollination_streamlit.api.client import ApiClient
 from pollination_streamlit.interactors import Recipe as ItRecipe
 from pollination_streamlit.interactors import NewJob, Job
-from pollination_streamlit.selectors import job_selector
 import zipfile
 
 def run_local_study(
@@ -93,27 +93,27 @@ def run_cloud_study(query: Query,
 
 def cloud_outputs(host: str, container):
     api_client = ApiClient(api_token=st.session_state.api_key)
-    job = job_selector(api_client) # is a st.text_input
+    with container:
+        job = job_selector(api_client) # is a st.text_input
+        if job and job.id != st.session_state.job_id:
+            st.session_state.job_id = job.id
 
-    if job and job.id != st.session_state.job_id:
-        st.session_state.job_id = job.id
+            run = job.runs[0]
+            input_model_path = job.runs_dataframe.dataframe['model'][0]
 
-        run = job.runs[0]
-        input_model_path = job.runs_dataframe.dataframe['model'][0]
+            simulation_folder = pathlib.Path(st.session_state.target_folder, 
+                run.id)
+            res_folder = simulation_folder.joinpath('cloud_results')
+            res_folder.mkdir(parents=True, exist_ok=True)
 
-        simulation_folder = pathlib.Path(st.session_state.target_folder, 
-            run.id)
-        res_folder = simulation_folder.joinpath('cloud_results')
-        res_folder.mkdir(parents=True, exist_ok=True)
+            # download results
+            res_zip = run.download_zipped_output('cumulative-sun-hours')
+            with zipfile.ZipFile(res_zip) as zip_folder:
+                zip_folder.extractall(res_folder.as_posix())
 
-        # download results
-        res_zip = run.download_zipped_output('cumulative-sun-hours')
-        with zipfile.ZipFile(res_zip) as zip_folder:
-            zip_folder.extractall(res_folder.as_posix())
-
-        model_dict = json.load(job.download_artifact(input_model_path))
-        st.session_state.hb_model_dict = model_dict
-        st.session_state.result_path = res_folder
+            model_dict = json.load(job.download_artifact(input_model_path))
+            st.session_state.hb_model_dict = model_dict
+            st.session_state.result_path = res_folder
 
     if st.session_state.result_path:
         get_vtk_model_result(st.session_state.hb_model_dict, 
