ce0cbb92c2a7aa2016b9adb5ff86a1e5b01eff25
Antonello Di Nunzio <plugin@antonellodinunzio.online>
2022-07-21 14:45:34 +0200

    fix(hbjson): write hbjson when loaded

--- simulation.py
+++ simulation.py
@@ -6,17 +6,22 @@ from typing import Optional
 from lbt_recipes.recipe import Recipe
 from honeybee.model import Model
 from ladybug.wea import Wea
+from query import Query
 from honeybee_radiance.config import folders as rad_folders
+from pollination_streamlit.api.client import ApiClient
+from pollination_streamlit.interactors import Recipe as ItRecipe
+from pollination_streamlit.interactors import NewJob
+
 
 def run_local_study(
-    here: str,
-    hb_model: Model,
-    wea_path: pathlib.Path,
-    timestep: Optional[int]=1, 
-    north: Optional[int]=0, 
-    cpu_count: Optional[int]=4, 
-    min_sensor_count: Optional[int]=100, 
-    grid_filter: Optional[str]='*'):
+        here: str,
+        hb_model: Model,
+        wea_path: pathlib.Path,
+        timestep: Optional[int]=1, 
+        north: Optional[int]=0, 
+        cpu_count: Optional[int]=4, 
+        min_sensor_count: Optional[int]=100, 
+        grid_filter: Optional[str]='*'):
 
     try:
         # reload wea
@@ -45,3 +50,33 @@ def run_local_study(
         st.session_state.results_path = None
         return False, e
 
+
+def run_cloud_study(query: Query,
+        api_client: ApiClient,
+        model_path: pathlib.Path,
+        wea_path: pathlib.Path):
+    query.job_id = None
+    # remember to add direct-sun-hours to your cloud project
+    recipe = ItRecipe('ladybug-tools', 'direct-sun-hours',
+                    '0.5.5-viz', api_client)
+
+    new_job = NewJob(query.owner,
+                     query.project,
+                     recipe,
+                     client=api_client)
+
+    model_project_path = new_job.upload_artifact(model_path)
+
+    wea_project_path = new_job.upload_artifact(wea_path)
+
+    q = {
+        'model': model_project_path,
+        'wea': wea_project_path
+    }
+
+    new_job.arguments = [q]
+    job = new_job.create() # run
+
+    query.job_id = job.id
+
+    return job.id
